45544db86a98901ac77ee58cd392721b0329c4d7
boba2fett <boba2002fett@gmail.com>
2019-05-02 20:54:12 +0200

    diagramm data

--- viergewIt/src/Testing.java
+++ viergewIt/src/Testing.java
@@ -3,23 +3,18 @@ import java.util.ArrayList;
 public class Testing
 {
     public static void main(String args[]) {
-        for(int w=4;w<10;w++)
+        int h=9;
+        for(int w=5;w<=9;w+=2)
         {
-            System.out.println("wwww "+w);
-            for(int h=4;h<10;h++)
+            System.out.println("w:"+w);
+            for(int d=1;d<10;d++)
             {
-                System.out.println("hhhhh "+h);
-                for(int d=1;d<16-w;d++)
-                {
-                    System.out.println("dddddd "+d);
-                    final long timeStart = System.currentTimeMillis();
-                    AiMiniMax a=new AiMiniMax(0,new ArrayList<Integer>(),w,h,d);
-                    int t=a.aiTurn();
-                    final long timeEnd = System.currentTimeMillis();
-                    double time=(timeEnd - timeStart)/1000.0;
-                    System.out.println("w "+w+" h "+h+" d "+d+" : " +time+"");
-                    System.out.println(t);
-                }
+                final long timeStart = System.currentTimeMillis();
+                AiMiniMax a=new AiMiniMax(0,new ArrayList<Integer>(),w,h,d);
+                a.aiTurn();
+                final long timeEnd = System.currentTimeMillis();
+                double time=(timeEnd - timeStart)/1000.0;
+                System.out.print(time+",");
             }
         }
     }
